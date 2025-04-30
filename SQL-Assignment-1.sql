08979034ff20ef55ac4c0256bccde5cbf1101475
r101409043 <34722067+r101409043@users.noreply.github.com>
2025-04-30 11:53:25 -0400

    Update SQL-Assignment-1.sql

--- SQL-Assignment-1.sql
+++ SQL-Assignment-1.sql
@@ -15,7 +15,7 @@ WHERE p.Color IS NULL;
 --Write a query that retrieves the columns ProductID, Name, Color and ListPrice from the Production.Product table, the rows that are not NULL for the Color column.
 SELECT p.ProductID, p.Name, p.Color, p.ListPrice
 FROM Production.Product p
-WHERE Color IS NOT NULL;
+WHERE p.Color IS NOT NULL;
 
 --Write a query that retrieves the columns ProductID, Name, Color and ListPrice from the Production.Product table, the rows that are not NULL for the column Color, and the column ListPrice has a value greater than zero.
 SELECT p.ProductID, p.Name, p.Color, p.ListPrice
